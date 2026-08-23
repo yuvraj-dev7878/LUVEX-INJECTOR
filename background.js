9f4c106eb74f75222e805914aa1fd48d77eb57d5
Yuvraj Singh <285486361+yuvraj-dev7878@users.noreply.github.com>
2026-08-23 13:27:40 +0530

    Create background.js

---
+++ background.js
@@ -0,0 +1,7 @@
+// TEAM LUVEX — Cookie Injector Background
+
+console.log('🍪 TEAM LUVEX Cookie Injector Background loaded');
+
+chrome.runtime.onInstalled.addListener(() => {
+  console.log('🍪 TEAM LUVEX Cookie Injector installed');
+});
